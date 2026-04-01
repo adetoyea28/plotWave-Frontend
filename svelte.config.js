4fa167be4fa46f5e5ff7be10523acb57e689609f
adetoyea28 <142647086+adetoyea28@users.noreply.github.com>
2026-04-01 06:09:49 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,11 @@
-import adapter from '@sveltejs/adapter-vercel'; // Change this line
-import { relative, sep } from 'node:path';
+import adapter from '@sveltejs/adapter-node';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		runes: ({ filename }) => {
-			const relativePath = relative(import.meta.dirname, filename);
-			const pathSegments = relativePath.toLowerCase().split(sep);
-			const isExternalLibrary = pathSegments.includes('node_modules');
-
-			return isExternalLibrary ? undefined : true;
-		}
-	},
-	kit: {
-		// This adapter handles the output directory for you automatically
-		adapter: adapter()
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter()
+    }
 };
-
 export default config;
